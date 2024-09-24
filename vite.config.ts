565196db505a8c138301173724062405701a4c3e
PierreLeBlond <pierre.lespingal@gmail.com>
2024-09-24 16:58:43 +0200

    fix: Set base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "https://pierreleblond.github.io/feis/build",
   resolve: {
     alias: {
       $lib: path.resolve("./src/lib"),
