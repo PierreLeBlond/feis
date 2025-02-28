534cbee6e1f2b76871016d128dfbe246db67e965
PierreLeBlond <pierre.lespingal@gmail.com>
2025-02-28 20:38:40 +0100

    fix: Do not use jekyll

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.svg"]),
+	assets: new Set([".nojekyll","favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.C4Eca3lj.js",app:"_app/immutable/entry/app.BYmY2JfT.js",imports:["_app/immutable/entry/start.C4Eca3lj.js","_app/immutable/chunks/CHfxqMlv.js","_app/immutable/chunks/DRF9QMb9.js","_app/immutable/chunks/DJLOelGB.js","_app/immutable/entry/app.BYmY2JfT.js","_app/immutable/chunks/DRF9QMb9.js","_app/immutable/chunks/C6vZ3gDl.js","_app/immutable/chunks/DRR3ANIp.js","_app/immutable/chunks/B1ENoBDp.js","_app/immutable/chunks/DJLOelGB.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.Bo3l45Bk.js",app:"_app/immutable/entry/app.DFn8vhLY.js",imports:["_app/immutable/entry/start.Bo3l45Bk.js","_app/immutable/chunks/DgAzfF3l.js","_app/immutable/chunks/DRF9QMb9.js","_app/immutable/chunks/DJLOelGB.js","_app/immutable/entry/app.DFn8vhLY.js","_app/immutable/chunks/DRF9QMb9.js","_app/immutable/chunks/C6vZ3gDl.js","_app/immutable/chunks/DRR3ANIp.js","_app/immutable/chunks/B1ENoBDp.js","_app/immutable/chunks/DJLOelGB.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
